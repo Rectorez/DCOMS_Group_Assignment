bb1271ccc293d07754f0d0b2f849ad5ab19d3416
RayTheTracer <72331788+RayTheTracer@users.noreply.github.com>
2022-08-22 09:52:18 +0500

    View Item Page backend done

--- src/main/java/Client/UI/ViewItemPage.java
+++ src/main/java/Client/UI/ViewItemPage.java
@@ -1,17 +1,31 @@
 package Client.UI;
 
+import Client.Main;
 import Client.UI.DesignUI;
 import Client.UI.DesignUI.DiscardBtn;
 import Client.UI.LoginPage;
 import Client.UI.MenuPage;
+import Client.UI.Utility.MyListCellRenderer;
+import Client.UI.Utility.MyTableCellRenderer;
+import InventoryPackage.Inventory;
+import InventoryPackage.InventoryHandler;
+import InventoryPackage.Item;
+import Server.InventoryInterface;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.rmi.RemoteException;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Vector;
+
+import static Client.Main.*;
 
 public class ViewItemPage extends JFrame implements ActionListener {
     JPanel rootPanel, selectPanel, contentPanel;
@@ -38,22 +52,29 @@ public class ViewItemPage extends JFrame implements ActionListener {
         selectLabel = new JLabel("Select Inventory");
         selectLabel.setFont(DesignUI.defaultFont);
 
-        invenComboBox = new JComboBox(new String[]{"Item 1", "Item 2"});
+        invenComboBox = new JComboBox();
         invenComboBox.setFont(DesignUI.defaultFont);
         invenComboBox.setBorder(new EmptyBorder(10,5,10,5));
+        invenComboBox.addActionListener(this);
+        invenComboBox.setRenderer(new MyListCellRenderer());
 
         selectPanel = new JPanel(new BorderLayout( 5, 5));
         selectPanel.add(selectLabel, BorderLayout.NORTH);
         selectPanel.add(invenComboBox, BorderLayout.CENTER);
 
-        table = new JTable(){
+        String col[] = {"ID", "Name", "Price", "Cost", "Stored Quantity", "Sold Quantity"};
+        DefaultTableModel tableModel = new DefaultTableModel(col, 0);
+
+        table = new JTable(tableModel){
             @Override
             public boolean isCellEditable(int row, int column) {
                 return false;
             }
         };
+
         table.setFont(DesignUI.defaultFont);
         table.setFillsViewportHeight(true);
+        table.setDefaultRenderer(Object.class, new MyTableCellRenderer());
 
         scrollPane = new JScrollPane(table);
 
@@ -71,6 +92,7 @@ public class ViewItemPage extends JFrame implements ActionListener {
         rootPanel.add(backBtn, BorderLayout.SOUTH);
 
         add(rootPanel);
+        setupComboboxes();
         setVisible(true);
     }
 
@@ -79,6 +101,32 @@ public class ViewItemPage extends JFrame implements ActionListener {
         if(e.getSource() == backBtn){
             new MenuPage();
             dispose();
+
+        }
+        if(e.getSource() == invenComboBox){
+            try {
+                DefaultTableModel model = (DefaultTableModel) table.getModel();
+                model.setRowCount(0);
+                InventoryInterface.GetItems((Inventory) invenComboBox.getSelectedItem()).stream().
+                        forEach(x -> model.addRow(new Object[]{x.GetID(), x.GetName(), x.GetPrice(), x.GetCost(),
+                                x.GetStoredQuantity(), x.GetSoldQuantity()}));
+                System.out.println(model.getRowCount());
+                table.setModel(model);
+            }
+            catch (RemoteException ex){
+                System.out.println("REMOTE EXCEPTION");;
+            }
+        }
+    }
+    private void setupComboboxes() {
+        try{
+            invenComboBox.removeAllItems();
+            Main.InventoryInterface.GetInventories().stream().forEach(x ->{
+                invenComboBox.addItem(x);
+            });
+        }
+        catch(RemoteException e){
+            System.out.println("REMOTE EXCEPTION");
         }
     }
 }
